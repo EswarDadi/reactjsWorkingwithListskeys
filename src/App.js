e6221110f9c687cf6128976945292ad526abe4a2
eswar <eshwardadi12@gmail.com>
2023-06-12 07:24:07 +0000

    userLists

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import UserProfile from './components/userDetails'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Eswar Dadi',
+    role: 'software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Eswar Dadi',
+    role: 'software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Eswar Dadi',
+    role: 'software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Eswar Dadi',
+    role: 'software Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">User Details</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails={eachItem} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
